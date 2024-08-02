b9f7fae290e0e76a60196d9fba12fa71ff8878a8
eden <2864416415@qq.com>
2024-08-02 12:21:53 +0800

    首页目录

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -24,7 +24,7 @@ const accumulation = () => {
 
 <template>
   <div :style="'--edge-theme-color: ' + color.themeColor + ';'">
-    <NCarousel class="w-100vw h-100vh" direction="vertical" :show-dots="false" mousewheel>
+    <NCarousel class="w-100vw h-100vh" direction="vertical" mousewheel>
       <div class="w-full h-full mid">
         <div @click="accumulation" class="select-none">
           <img src="/src/assets/logop.png" />
@@ -42,6 +42,16 @@ const accumulation = () => {
       <div class="w-full h-full flex justify-center items-center">
         <div>...</div>
       </div>
+      <template #dots="{ total, currentIndex, to }">
+        <ul class="custom-dots">
+          <li
+            v-for="index of total"
+            :key="index"
+            :class="{ ['is-active']: currentIndex === index - 1 }"
+            @click="to(index - 1)"
+          />
+        </ul>
+      </template>
     </NCarousel>
     <div class="bottom">
       <VersionNumber :versionNumber="'鲁ICP备2022011940号-1'" :color="color.themeColor" />
@@ -76,7 +86,33 @@ const accumulation = () => {
   bottom: 0;
   width: 100%;
 }
-p a i {
+.custom-dots {
+  display: flex;
+  flex-flow: column nowrap;
+  margin: 0;
+  padding: 0;
+  position: absolute;
+  top: 2vh;
+  height: 93vh;
+  right: 1vw;
+}
+
+.custom-dots li {
+  display: block;
+  height: 62vh;
+  width: 7px;
+  margin: 7px 0;
+  border-radius: 7px;
   cursor: pointer;
+  transition:
+    flex 0.7s,
+    background-color 0.21s cubic-bezier(0.4, 0, 0.2, 1);
+  flex: auto;
+  background-color: var(--color-border-hover);
+}
+
+.custom-dots li.is-active {
+  flex: none;
+  background: var(--edge-theme-color);
 }
 </style>
